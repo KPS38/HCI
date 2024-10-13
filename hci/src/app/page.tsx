be68f141f0e4cf92a4cf9f38b4a9fba8795c0b60
KPS38 <kristijanps@outlook.com>
2024-10-13 16:45:55 +0200

    Prvi commit

---
+++ hci/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{textAlign: "center"}}>
+      <h1 style={{padding: "8rem 0", fontSize: "8rem", fontWeight: 900}}>
+        Home page
+        </h1>
+    </div>
+  );
+}
